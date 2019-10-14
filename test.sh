f24ee06ff8937f0ea979ee3808d42f5c757a1fb9
ZhaoXiuting <43334810+ZhaoXiuting@users.noreply.github.com>
2019-10-14 20:52:40 +0800

    revise test.sh:require data of order

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ User=root
 PW=Ise_Nel_2017
 sql=" SELECT commitId from gitLog WHERE STATUS = 'F' ORDER BY commitId desc"
 #echo $sql
-result=` mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 commit_test -e "${sql}" `
+result=` mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 weekly_test -e "${sql}" `
 #result=`mysql -h $Host -u $User -pweekly_test <<EOF
 #SELECT commitId from gitLog WHERE STATUS = 'F'
 #EOF`
@@ -17,13 +17,15 @@ echo $i >> commitId.txt
 done
 #cat commitId.txt
 secommit=secommit_id
-j=2
-commitid=$(awk "NR==$j""{print $1}" commitId.txt)
+number=$(awk 'END{print NR}' commitId.txt)
+echo $number
+commitid=$(awk "NR==$number""{print $1}" commitId.txt)
 echo $commitid
+cd 
 sed -i 's/'$secommit'/'$commitid'/g' cli-benchmark.sh
 #jia zhixingchaxunde jiaobencichu
 echo "commitId is $commitid"
-exec ./ciscripts/fit/ingestion-overflow50-auto-test.sh
+./ciscripts/fit/ingestion-overflow50-auto-test.sh
 sed -i 's/'$commitid'/'$secommit'/g' cli-benchmark.sh
 rm -rf commitId.txt
 #ma1=$(echo $result | sed 's/.*commitId/commitId/')
