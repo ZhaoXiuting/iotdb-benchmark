4511a110e53dda3c722f8d06a3626fc8d03fb3c1
ZhaoXiuting <43334810+ZhaoXiuting@users.noreply.github.com>
2019-10-14 17:13:40 +0800

    test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ User=root
 PW=Ise_Nel_2017
 sql=" SELECT commitId from gitLog WHERE STATUS = 'F' ORDER BY commitId desc"
 #echo $sql
-result=` mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 weekly_test -e "${sql}" `
+result=` mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 commit_test -e "${sql}" `
 #result=`mysql -h $Host -u $User -pweekly_test <<EOF
 #SELECT commitId from gitLog WHERE STATUS = 'F'
 #EOF`
@@ -23,7 +23,7 @@ echo $commitid
 sed -i 's/'$secommit'/'$commitid'/g' cli-benchmark.sh
 #jia zhixingchaxunde jiaobencichu
 echo "commitId is $commitid"
-./ciscripts/fit/ingestion-overflow50-auto-test.sh
+exec ./ciscripts/fit/ingestion-overflow50-auto-test.sh
 sed -i 's/'$commitid'/'$secommit'/g' cli-benchmark.sh
 rm -rf commitId.txt
 #ma1=$(echo $result | sed 's/.*commitId/commitId/')
