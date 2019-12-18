c2e980e8094207d2ee31d6db80eb43efc5d2ad5c
ZhaoXiuting <1770228145@qq.com>
2019-12-18 10:49:51 +0800

    reise fuhao

--- revise_commitId.sh
+++ revise_commitId.sh
@@ -4,14 +4,14 @@ User=root
 PW=Ise_Nel_2017
 sql=" SELECT commitId from gitLog WHERE STATUS = 'F' "
 #echo $sql
-result=` mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 commit_test -e "${sql}" `
+result=`mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 commit_test -e "${sql}"`
 #result=`mysql -h $Host -u $User -pweekly_test <<EOF
 #SELECT commitId from gitLog WHERE STATUS = 'F'
 #EOF`
 #echo ${result}>test.txt
 #m=$(awk 'END{print NR}' test.txt)
 #echo
-if [ -z "${result}" ]; then
+if [ -z "${result}" ];then
     echo "没有搜索到状态为F的版本号"
 else
     for i in $result
