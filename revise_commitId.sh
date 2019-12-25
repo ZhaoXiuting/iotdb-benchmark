0fb43f1f74eca4285aa3913754ed03ecdd939229
ZhaoXiuting <1770228145@qq.com>
2019-12-26 01:51:12 +0800

    iii

--- revise_commitId.sh
+++ revise_commitId.sh
@@ -11,25 +11,25 @@ result=`mysql -h 192.168.130.19 -P3306 -uroot -pIse_Nel_2017 commit_test -e "${s
 #echo ${result}>test.txt
 #m=$(awk 'END{print NR}' test.txt)
 #echo
-#if [ -z "${result}" ];then
-for i in $result
-do
-   if [ -z "${i}" ];then
-   echo "It is null"
-   else
-   echo $i >> commitId.txt
-   secommit=secommit_id
-   number=2
+if [ -z "${result}" ];then
+    echo "没有搜索到状态为F的版本号"
+else
+    for i in $result
+    do
+    echo $i >> commitId.txt
+    done
+secommit=secommit_id
+number=2
 #number=$(awk 'END{print NR}' commitId.txt)
-    echo -e "\033[32m------------------------\033[1m"
+echo -e "\033[32m------------------------\033[1m"
 #echo $number
-    commitid=$(awk "NR==$number""{print $1}" commitId.txt)
-    echo $commitid
-    sed -i 's/'$secommit'/'$commitid'/g' cli-benchmark.sh
-    echo "commitId is $commitid"
-    echo -e "\033[32m------------------------\033[0m"
-    source ./ciscripts/fit/ingestion-overflow50-auto-test.sh
-    sed -i 's/'$commitid'/'$secommit'/g' cli-benchmark.sh
-    rm -rf commitId.txt
-    fi
-done
+commitid=$(awk "NR==$number""{print $1}" commitId.txt)
+echo $commitid
+sed -i 's/'$secommit'/'$commitid'/g' cli-benchmark.sh
+#jia zhixingchaxunde jiaobencichu
+echo "commitId is $commitid"
+echo -e "\033[32m------------------------\033[0m"
+source ./ciscripts/fit/ingestion-overflow50-auto-test.sh
+sed -i 's/'$commitid'/'$secommit'/g' cli-benchmark.sh
+rm -rf commitId.txt
+fi
